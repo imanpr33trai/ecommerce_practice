02ed03acdb48273ac780bf1668909888b63697f9
Manpreet Singh <manpreet.singh01356@gmail.com>
2026-01-19 12:05:10 +0530

    4added some build script and transpiler and starting the deploy2

--- apps/server/build.ts
+++ apps/server/build.ts
@@ -1,21 +1,19 @@
+// build.ts
 import { build } from "esbuild";
 
 await build({
   entryPoints: ["src/index.ts"],
   outfile: "dist/index.js",
-
   bundle: true,
   platform: "node",
   format: "esm",
   target: "es2022",
-
   sourcemap: true,
   minify: true,
 
-  // VERY IMPORTANT: do NOT bundle these
-  external: ["@ecomerceNextjs/*", "@prisma/client", "@prisma/adapter-pg", ".prisma/client"],
+  // ADD 'hono' TO EXTERNAL
+  // This allows Vercel's scanner to detect the hono dependency in the final bundle
+  external: ["hono", "@ecomerceNextjs/*", "@prisma/client", "@prisma/adapter-pg", ".prisma/client"],
 
   logLevel: "info",
 });
-
-console.log("✅ Server build completed");
